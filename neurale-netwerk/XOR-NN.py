36b1c553677f9348b25d28dc15e7d80753f329a4
Sinem ertem <wolfsinem@mbp-van-sinem.home>
2020-02-12 22:37:05 +0100

    add

--- neurale-netwerk/XOR-NN.py
+++ neurale-netwerk/XOR-NN.py
@@ -27,10 +27,12 @@ def sigmoid_function(self,x):
     """sigmoid function for hidden layer"""
     return 1.0/(1.0+np.exp(-x))
     
+
 def sigmoid_derivative(self,x):
     """derivative of the sigmoid function"""
     return x * (1 - x)
         
+
 class Neuron:
     """
     representing each neuron in the network.
@@ -39,40 +41,59 @@ class Neuron:
         - compute the output w sigmoid 
     """
 
-    def __init__(self, input_neuron, weight_neuron):
+    def __init__(self, input_neuron, weight_neuron, bias_weight):
         """
-        param_input_neuron = given input in the first layer
-        param_weight = factor of how important each neuron is
+        param: input_neuron = given input in the first layer
+        param: weight_neuron = factor of how important each neuron is
+        param: bias_weight = 
         """
-
         self.input_neuron = input_neuron 
         self.weight_neuron = weight_neuron
+        self.bias_weight = rand()
 
-        self.bias = rand()
         self.last_output = 0
         self.last_input = []
 
-    
-    def get_output(self,inputs):
+
+    def get_output(self):
         """output of a neuron f = (w1 * X1 +/- bias) + (w2 * X2 +/- bias)"""
-
-        self.last_input = inputs
-
         output = 0
-        for i in range(len(inputs)):
+        for i in inputs:
             output = output + inputs[i] * self.weight_neuron[i]
         output = output + self.bias
 
         self.last_output = sigmoid_function(output)
         return self.last_output
 
+
+    def set_output(self):
+        """only used for setting fixed output of neurons on input layer"""
+        pass 
+
+
+class Layer: 
+    def __init__(self, name, neurons)
+        self.name = name 
+        self.neurons = neurons 
+        pass
+
+
 class Neural_network:
     """Initializing the Neural Network"""
 
     def __init__(self, input_layer = 2, hidden_layer = 2, output_layer= 1):
-        """all the layers in the neural network"""
+    """
+    Builds a neural network of neurons, topology is a list of layers, with 
+    number of neurons per layer; e.g.
+    """"
         self.input_layer = input_layer
         self.hidden_layer = hidden_layer
         self.output_layer = output_layer
 
         hidden_weights = np.random.uniform(size=(self.input_layer,self.hidden_layer))
+        pass
+
+    
+    def forward_pass(self, input):
+        """Calculates the feedforward of the network when presented with input."""
+        pass
